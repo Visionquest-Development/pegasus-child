01c748253b4c9f30b74f0723f7c87e9f499932c4
Jimobrien <jim.obrien3@gmail.com>
2024-07-09 18:28:28 -0400

    Outlaw Coffee First Commit
    Outlaw Coffee First Commit

---
+++ tpl_home.php
@@ -0,0 +1,110 @@
+<?php
+/* 
+	Template Name: Home Template
+*/
+?>
+	<?php get_header(); ?>
+	
+	<div id="page-wrap">
+		<?php
+			//full container page options
+			$post_full_container_choice = get_post_meta( get_the_ID(), 'pegasus-page-container-checkbox', true );
+			//full container theme option
+			$global_full_container_option = pegasus_get_option('full_container_chk' );
+
+			//assign post class
+			$pegasus_post_container_choice = ( 'on' === $post_full_container_choice ) ? 'container-fluid' : 'container';
+			//assign global class
+			$pegasus_global_container_choice = ( 'on' === $global_full_container_option ) ? 'container-fluid' : 'container' ;
+			//check global first then post
+			$final_container_class = ( 'container-fluid' === $pegasus_global_container_choice ) ? $pegasus_global_container_choice : $pegasus_post_container_choice;
+
+			//left align right sidebar?
+			$left_align_sidebar_chk =  pegasus_get_option( 'sidebar_left_chk' ) ? pegasus_get_option( 'sidebar_left_chk' ) : 'off';
+			//enable both sidebars?
+			$pegasus_left_sidebar_option = ( 'on' === pegasus_get_option( 'both_sidebar_chk' ) ) ? pegasus_get_option( 'both_sidebar_chk' ) : 'off';
+			//change content class if both sidebars
+			$page_body_content_class = ( 'on' === $pegasus_left_sidebar_option  ) ? 'col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xg-6' : 'col-xs-12 col-sm-12 col-md-12 col-lg-9 col-xg-9';
+
+			//page header page options
+			$post_disable_page_header_choice = get_post_meta( get_the_ID(), 'pegasus-page-header-checkbox', true ) ? get_post_meta( get_the_ID(), 'pegasus-page-header-checkbox', true ) : 'off';
+			//page header theme option
+			$global_disable_page_header_option =  pegasus_get_option('page_header_chk' ) ? pegasus_get_option('page_header_chk' ) : 'off';
+			//check theme option for page header before page option
+			$page_title = $post->post_title;
+			$is_this_home = is_home();
+			if ( 'on' === $global_disable_page_header_option ) {
+				$final_page_header_option = 'on';
+			} elseif ( 'on' === $post_disable_page_header_choice ) {
+				$final_page_header_option = 'on';
+			} else {
+				$final_page_header_option = 'off';
+			}
+
+			if ( true === $is_this_home ) {
+				$final_page_header_option = 'off';
+			}
+		?>
+		
+		<div class="">
+			<div class="home-hero-slider">
+				<?php /*
+				echo do_shortcode( '[slider]
+				[slide class="testing"]<img class="alignnone size-full wp-image-12" src="https://test.outlawcoffeecompany.com/wp-content/uploads/2024/07/slider2.jpg" alt="Gold-and-Black-Logo">[/slide]
+				[slide]<img class="alignnone size-full wp-image-12" src="https://test.outlawcoffeecompany.com/wp-content/uploads/2024/07/slider3.jpg" alt="Gold-and-Black-Logo">[/slide]
+				[/slider]' ); */
+				?>
+			</div>
+		</div>
+		
+		<section class="jumbotron text-center">
+			<div class="container">
+				<h1 class="jumbotron-heading">A Legacy of Service and Excellence</h1>
+				<p class="lead text-muted">Built on a foundation of service and excellence, Outlaw Coffee Company embodies the commitment and courage of our veteran roots. We serve more than just coffee; we serve a community bound by respect, honor, and a shared love for quality. Join us in celebrating a legacy that goes beyond the cup—a legacy that honors the past, embraces the present, and brews a brighter future. Outlaw Coffee Company isn’t just about what’s in your cup; it’s about the spirit with which it’s made.</p>
+				
+				<p>
+					<a href="/online-shop/" class="btn btn-primary my-2">CLICK HERE FOR THE ONLINE SHOP</a>
+					
+				</p>
+			</div>
+		</section>
+		
+		<div class="<?php echo $final_container_class; ?>">
+		<!-- Example row of columns -->
+			<div class="">
+		
+				<div class="inner-content">	
+					<div class="content-no-sidebar">
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+							<?php if( 'off' === $final_page_header_option ) { ?>
+								<div class="page-header">
+									<?php
+									if( '' === $page_title ) {
+										echo '';
+									} elseif ( $page_title ) {
+										echo '<h1>';
+										echo the_title();
+										echo '</h1>';
+									}
+									?>
+								</div>
+							<?php }else{ ?>
+								<div class="page-header-spacer"></div>
+							<?php } ?>
+
+							<?php the_content(); ?>
+						
+						<?php endwhile; else: ?>
+							<?php /* kinda a 404 of sorts when not working */ ?>
+							<div class="page-header">
+								<h1>Oh no!</h1>
+							</div>
+							<p>No content is appearing for this page!</p>
+						<?php endif; ?>
+					</div>
+				</div><!--end inner content-->
+
+			</div><!--end row -->
+		</div><!-- end container -->
+	</div><!-- end page wrap -->
+    <?php get_footer(); ?>
