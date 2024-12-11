44b924adc7609e286a0e50878cb9c21a3a7f7cc0
Jimobrien <jim.obrien3@gmail.com>
2024-12-11 02:47:41 -0500

    Update pegasus-custom.js

--- js/pegasus-custom.js
+++ js/pegasus-custom.js
@@ -30,7 +30,7 @@
 	});
 
 
-	jQuery(window).load(function($) {
+	jQuery(window).on( 'load', function($) {
 		// executes when complete page is fully loaded, including all frames, objects and images
 		//alert("window is loaded");
 	});
