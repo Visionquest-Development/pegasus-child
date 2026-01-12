5b3b6d779f375cc6fa0cbc78c64c60fb01a4e13e
Jimobrien <jim.obrien3@gmail.com>
2026-01-11 22:18:19 -0500

    mabellas_theme first commit

--- functions.php
+++ functions.php
@@ -30,3 +30,402 @@
 
 	} //end function
 	add_action( 'wp_enqueue_scripts', 'pegasus_child_bootstrap_js' );
+
+
+	/**
+	 * Homepage Sections metabox (CMB2) – shown only on the static front page
+	 */
+	if ( ! function_exists( 'cmb2_homepage_metabox' ) ) {
+		function cmb2_homepage_metabox() {
+
+			$homepage_id = (int) get_option( 'page_on_front' ); // ID of the static front page
+			if ( ! $homepage_id ) {
+				return; // bail if a front page is not set
+			}
+
+			$prefix = 'homepage_sections_';
+
+			$cmb = new_cmb2_box( array(
+				'id'            => $prefix . 'metabox',
+				'title'         => __( 'Homepage Sections', 'pegasus-bootstrap' ),
+				'object_types'  => array( 'page' ),
+				'show_on'       => array(
+					'key'   => 'id',
+					'value' => array( $homepage_id ),
+				),
+				'context'       => 'normal',
+				'priority'      => 'high',
+				'show_names'    => true,
+			) );
+
+			$group_field_id = $cmb->add_field( array(
+				'id'          => $prefix . 'repeatable_group',
+				'type'        => 'group',
+				'description' => __( 'Add multiple sections for the homepage', 'pegasus-bootstrap' ),
+				'options'     => array(
+					'group_title'   => __( 'Section {#}', 'pegasus-bootstrap' ),
+					'add_button'    => __( 'Add Another Section', 'pegasus-bootstrap' ),
+					'remove_button' => __( 'Remove Section', 'pegasus-bootstrap' ),
+					'sortable'      => true,
+				),
+			) );
+
+			// Background image
+			$cmb->add_group_field( $group_field_id, array(
+				'name'    => __( 'Background Image', 'pegasus-bootstrap' ),
+				'id'      => 'background_image',
+				'type'    => 'file',
+				'options' => array(
+					'url' => false, // hide URL input
+				),
+				'text'    => array(
+					'add_upload_file_text' => __( 'Add Background Image', 'pegasus-bootstrap' ),
+				),
+			) );
+
+			// Title
+			$cmb->add_group_field( $group_field_id, array(
+				'name' => __( 'Title', 'pegasus-bootstrap' ),
+				'id'   => 'title',
+				'type' => 'text',
+			) );
+
+			// Subtitle
+			$cmb->add_group_field( $group_field_id, array(
+				'name' => __( 'Subtitle', 'pegasus-bootstrap' ),
+				'id'   => 'subtitle',
+				'type' => 'text',
+			) );
+
+			// Paragraph
+			$cmb->add_group_field( $group_field_id, array(
+				'name' => __( 'Paragraph', 'pegasus-bootstrap' ),
+				'id'   => 'paragraph',
+				'type' => 'wysiwyg',
+			) );
+
+			// Button 1 Text
+			$cmb->add_group_field( $group_field_id, array(
+				'name' => __( 'Button 1 Text', 'pegasus-bootstrap' ),
+				'id'   => 'button1_text',
+				'type' => 'text',
+			) );
+
+			// Button 1 Link
+			$cmb->add_group_field( $group_field_id, array(
+				'name' => __( 'Button 1 Link', 'pegasus-bootstrap' ),
+				'id'   => 'button1_link',
+				'type' => 'text_url',
+			) );
+		}
+	}
+	add_action( 'cmb2_admin_init', 'cmb2_homepage_metabox' );
+
+	/**
+	 * Helper: normalize CMB2 file field (ID / array / URL) to a URL
+	 */
+	if ( ! function_exists( 'mabellas_get_cmb2_image_url' ) ) {
+		function mabellas_get_cmb2_image_url( $value ) {
+
+			if ( empty( $value ) ) {
+				return '';
+			}
+
+			// CMB2 can give us an array with 'id' and/or 'url'
+			if ( is_array( $value ) ) {
+				if ( ! empty( $value['url'] ) ) {
+					return esc_url( $value['url'] );
+				}
+				if ( ! empty( $value['id'] ) ) {
+					$value = (int) $value['id'];
+				}
+			}
+
+			// Attachment ID
+			if ( is_numeric( $value ) ) {
+				$src = wp_get_attachment_image_src( (int) $value, 'full' );
+				if ( $src && ! empty( $src[0] ) ) {
+					return esc_url( $src[0] );
+				}
+			}
+
+			// Plain URL
+			return esc_url( $value );
+		}
+	}
+
+	/**
+	 * Register Locations Custom Post Type + Taxonomies
+	 */
+	function mabellas_register_locations_cpt() {
+
+		/*============================
+		======= Locations Post Type =======
+		============================*/
+
+		$locations_labels = array(
+			'name'               => _x( 'Locations', 'locations general name', 'pegasus-bootstrap' ),
+			'singular_name'      => _x( 'Location', 'locations singular name', 'pegasus-bootstrap' ),
+			'add_new'            => _x( 'Add New', 'location', 'pegasus-bootstrap' ),
+			'add_new_item'       => __( 'Add New Location', 'pegasus-bootstrap' ),
+			'edit_item'          => __( 'Edit Location', 'pegasus-bootstrap' ),
+			'new_item'           => __( 'New Location', 'pegasus-bootstrap' ),
+			'view_item'          => __( 'View Location', 'pegasus-bootstrap' ),
+			'search_items'       => __( 'Search Locations', 'pegasus-bootstrap' ),
+			'not_found'          => __( 'No locations found', 'pegasus-bootstrap' ),
+			'not_found_in_trash' => __( 'No locations found in Trash', 'pegasus-bootstrap' ),
+			'parent_item_colon'  => '',
+			'menu_name'          => __( 'Locations', 'pegasus-bootstrap' ),
+		);
+
+		$locations_args = array(
+			'labels'             => $locations_labels,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'capability_type'    => 'post',
+			'can_export'         => true,
+			'has_archive'        => false,
+			'rewrite'            => array( 'slug' => 'locations' ),
+			'hierarchical'       => false,
+			'menu_position'      => null,
+			'supports'           => array( 'title', 'editor', 'thumbnail', 'custom-fields' ),
+			'show_in_rest'       => true,
+			'menu_icon'          => 'dashicons-location-alt',
+		);
+
+		register_post_type( 'locations', $locations_args );
+
+		// Remove supports you don't want
+		remove_post_type_support( 'locations', 'author' );
+		remove_post_type_support( 'locations', 'excerpt' );
+		remove_post_type_support( 'locations', 'trackbacks' );
+
+		/*============================
+		======= Taxonomy: Tags =======
+		============================*/
+
+		$location_tags_labels = array(
+			'name'              => _x( 'Location Tags', 'taxonomy general name', 'pegasus-bootstrap' ),
+			'singular_name'     => _x( 'Location Tag', 'taxonomy singular name', 'pegasus-bootstrap' ),
+			'search_items'      => __( 'Search Location Tags', 'pegasus-bootstrap' ),
+			'all_items'         => __( 'All Location Tags', 'pegasus-bootstrap' ),
+			'parent_item'       => __( 'Parent Location Tag', 'pegasus-bootstrap' ),
+			'parent_item_colon' => __( 'Parent Location Tag:', 'pegasus-bootstrap' ),
+			'edit_item'         => __( 'Edit Location Tag', 'pegasus-bootstrap' ),
+			'update_item'       => __( 'Update Location Tag', 'pegasus-bootstrap' ),
+			'add_new_item'      => __( 'Add New Location Tag', 'pegasus-bootstrap' ),
+			'new_item_name'     => __( 'New Location Tag Name', 'pegasus-bootstrap' ),
+			'menu_name'         => __( 'Location Tags', 'pegasus-bootstrap' ),
+		);
+
+		register_taxonomy(
+			'location_tags',
+			array( 'locations' ),
+			array(
+				'hierarchical'      => false,
+				'labels'            => $location_tags_labels,
+				'show_ui'           => true,
+				'show_admin_column' => true,
+				'query_var'         => true,
+				'rewrite'           => array( 'slug' => 'location-tags' ),
+			)
+		);
+
+		/*============================
+		======= Taxonomy: Categories =======
+		============================*/
+
+		$location_cats_labels = array(
+			'name'              => _x( 'Location Categories', 'taxonomy general name', 'pegasus-bootstrap' ),
+			'singular_name'     => _x( 'Location Category', 'taxonomy singular name', 'pegasus-bootstrap' ),
+			'search_items'      => __( 'Search Location Categories', 'pegasus-bootstrap' ),
+			'all_items'         => __( 'All Location Categories', 'pegasus-bootstrap' ),
+			'parent_item'       => __( 'Parent Location Category', 'pegasus-bootstrap' ),
+			'parent_item_colon' => __( 'Parent Location Category:', 'pegasus-bootstrap' ),
+			'edit_item'         => __( 'Edit Location Category', 'pegasus-bootstrap' ),
+			'update_item'       => __( 'Update Location Category', 'pegasus-bootstrap' ),
+			'add_new_item'      => __( 'Add New Location Category', 'pegasus-bootstrap' ),
+			'new_item_name'     => __( 'New Location Category Name', 'pegasus-bootstrap' ),
+			'menu_name'         => __( 'Location Categories', 'pegasus-bootstrap' ),
+		);
+
+		register_taxonomy(
+			'location_categories',
+			array( 'locations' ),
+			array(
+				'hierarchical'      => true,
+				'labels'            => $location_cats_labels,
+				'show_ui'           => true,
+				'show_admin_column' => true,
+				'query_var'         => true,
+				'rewrite'           => array( 'slug' => 'location-categories' ),
+			)
+		);
+	}
+	add_action( 'init', 'mabellas_register_locations_cpt' );
+
+	/**
+	 * CMB2 metabox for Locations CPT
+	 */
+	function mabellas_locations_metabox() {
+
+		$prefix = 'mabellas_location_';
+
+		$cmb = new_cmb2_box(
+			array(
+				'id'           => $prefix . 'details',
+				'title'        => __( 'Location Details', 'pegasus-bootstrap' ),
+				'object_types' => array( 'locations' ),
+				'context'      => 'normal',
+				'priority'     => 'high',
+				'show_names'   => true,
+			)
+		);
+
+		// Display name (if different from post title)
+		$cmb->add_field(
+			array(
+				'name' => __( 'Display Name', 'pegasus-bootstrap' ),
+				'desc' => __( 'Optional. If empty, the post title will be used.', 'pegasus-bootstrap' ),
+				'id'   => $prefix . 'display_name',
+				'type' => 'text',
+			)
+		);
+
+		// Address fields
+		$cmb->add_field(
+			array(
+				'name' => __( 'Street Address', 'pegasus-bootstrap' ),
+				'id'   => $prefix . 'street',
+				'type' => 'text',
+			)
+		);
+
+		$cmb->add_field(
+			array(
+				'name' => __( 'Address Line 2', 'pegasus-bootstrap' ),
+				'id'   => $prefix . 'street2',
+				'type' => 'text',
+			)
+		);
+
+		$cmb->add_field(
+			array(
+				'name' => __( 'City', 'pegasus-bootstrap' ),
+				'id'   => $prefix . 'city',
+				'type' => 'text',
+				'default' => 'Columbus',
+			)
+		);
+
+		$cmb->add_field(
+			array(
+				'name'    => __( 'State', 'pegasus-bootstrap' ),
+				'id'      => $prefix . 'state',
+				'type'    => 'text_small',
+				'default' => 'GA',
+			)
+		);
+
+		$cmb->add_field(
+			array(
+				'name' => __( 'Postal Code', 'pegasus-bootstrap' ),
+				'id'   => $prefix . 'zip',
+				'type' => 'text_small',
+			)
+		);
+
+		// Phone (human display)
+		$cmb->add_field(
+			array(
+				'name' => __( 'Phone (Display)', 'pegasus-bootstrap' ),
+				'desc' => __( 'e.g. (706) 940-0070', 'pegasus-bootstrap' ),
+				'id'   => $prefix . 'phone_display',
+				'type' => 'text',
+			)
+		);
+
+		// Phone (tel link)
+		$cmb->add_field(
+			array(
+				'name' => __( 'Phone (tel value)', 'pegasus-bootstrap' ),
+				'desc' => __( 'e.g. +17069400070 (digits only, with country code)', 'pegasus-bootstrap' ),
+				'id'   => $prefix . 'phone_tel',
+				'type' => 'text',
+			)
+		);
+
+		// Optional secondary phone
+		$cmb->add_field(
+			array(
+				'name' => __( 'Secondary Phone (Display)', 'pegasus-bootstrap' ),
+				'id'   => $prefix . 'phone2_display',
+				'type' => 'text',
+			)
+		);
+
+		$cmb->add_field(
+			array(
+				'name' => __( 'Secondary Phone (tel value)', 'pegasus-bootstrap' ),
+				'id'   => $prefix . 'phone2_tel',
+				'type' => 'text',
+			)
+		);
+
+		// URLs
+		$cmb->add_field(
+			array(
+				'name' => __( 'Google Maps URL', 'pegasus-bootstrap' ),
+				'id'   => $prefix . 'maps_url',
+				'type' => 'text_url',
+			)
+		);
+
+		$cmb->add_field(
+			array(
+				'name' => __( 'Reservation URL', 'pegasus-bootstrap' ),
+				'desc' => __( 'OpenTable or reservation system link (optional)', 'pegasus-bootstrap' ),
+				'id'   => $prefix . 'reservation_url',
+				'type' => 'text_url',
+			)
+		);
+
+		// Card / Front-end display meta
+		$cmb->add_field(
+			array(
+				'name' => __( 'Card Background Image', 'pegasus-bootstrap' ),
+				'desc' => __( 'Used for the locations grid on the homepage.', 'pegasus-bootstrap' ),
+				'id'   => $prefix . 'card_background_image',
+				'type' => 'file',
+				'options' => array(
+					'url' => false,
+				),
+				'text' => array(
+					'add_upload_file_text' => __( 'Add Card Image', 'pegasus-bootstrap' ),
+				),
+			)
+		);
+
+		$cmb->add_field(
+			array(
+				'name'    => __( 'Card Button Text', 'pegasus-bootstrap' ),
+				'desc'    => __( 'e.g. VISIT US', 'pegasus-bootstrap' ),
+				'id'      => $prefix . 'card_button_text',
+				'type'    => 'text',
+				'default' => 'Visit Us',
+			)
+		);
+
+		$cmb->add_field(
+			array(
+				'name' => __( 'Card Button Link', 'pegasus-bootstrap' ),
+				'desc' => __( 'If empty, can link to the single location page.', 'pegasus-bootstrap' ),
+				'id'   => $prefix . 'card_button_link',
+				'type' => 'text_url',
+			)
+		);
+	}
+	add_action( 'cmb2_admin_init', 'mabellas_locations_metabox' );
---
+++ tpl_home.php
@@ -0,0 +1,330 @@
+<?php
+/*
+	Template Name: Home Template
+*/
+?>
+	<?php get_header(); ?>
+
+	<?php
+		$header_choice = pegasus_get_option( 'header_select' );
+		//var_dump($header_choice);
+		if ( 'header-three' === $header_choice ) {
+			get_template_part( 'templates/additional_header' );
+		}
+	?>
+
+	<div id="page-wrap">
+
+		<?php
+			//full container page options
+			$post_full_container_choice = get_post_meta( get_the_ID(), 'pegasus-page-container-checkbox', true );
+			//full container theme option
+			$global_full_container_option = pegasus_get_option('full_container_chk' );
+
+			//assign post class
+			$pegasus_post_container_choice = ( 'on' === $post_full_container_choice ) ? 'container-fluid' : 'container';
+			//assign global class
+			$pegasus_global_container_choice = ( 'on' === $global_full_container_option ) ? 'container-fluid' : 'container' ;
+			//check global first then post
+			$final_container_class = ( 'container-fluid' === $pegasus_global_container_choice ) ? $pegasus_global_container_choice : $pegasus_post_container_choice;
+
+			//left align right sidebar?
+			$left_align_sidebar_chk =  pegasus_get_option( 'sidebar_left_chk' ) ? pegasus_get_option( 'sidebar_left_chk' ) : 'off';
+			//enable both sidebars?
+			$pegasus_left_sidebar_option = ( 'on' === pegasus_get_option( 'both_sidebar_chk' ) ) ? pegasus_get_option( 'both_sidebar_chk' ) : 'off';
+			//change content class if both sidebars
+			$page_body_content_class = ( 'on' === $pegasus_left_sidebar_option  ) ? 'col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xg-6' : 'col-xs-12 col-sm-12 col-md-12 col-lg-9 col-xg-9';
+
+			//page header page options
+			$post_disable_page_header_choice = get_post_meta( get_the_ID(), 'pegasus-page-header-checkbox', true ) ? get_post_meta( get_the_ID(), 'pegasus-page-header-checkbox', true ) : 'off';
+			//page header theme option
+			$global_disable_page_header_option =  pegasus_get_option('page_header_chk' ) ? pegasus_get_option('page_header_chk' ) : 'off';
+			//check theme option for page header before page option
+			$page_title = $post->post_title;
+			$is_this_home = is_home();
+			if ( 'on' === $global_disable_page_header_option ) {
+				$final_page_header_option = 'on';
+			} elseif ( 'on' === $post_disable_page_header_choice ) {
+				$final_page_header_option = 'on';
+			} else {
+				$final_page_header_option = 'off';
+			}
+
+			if ( true === $is_this_home ) {
+				$final_page_header_option = 'off';
+			}
+		?>
+		
+		<div class="container-fluid my-5 home-locations" >
+			<h2 class="text-white text-center pb-3">Locations</h2>
+			<div class="row g-3">
+
+				<!-- UPTOWN COLUMBUS -->
+				<div class="col-lg-6">
+					<div class="mabellas-location-card text-center h-100 p-3 text-white">
+
+						<div class="mabellas-location-image"
+							 style="background-image:url('https://visionquestdev.com/mabellas/wp-content/uploads/2025/12/INYtFcBSnyge5MW6Z7bQ_Mabellas-62.png');">
+						</div>
+
+						<h2>UPTOWN COLUMBUS</h2>
+
+						<a href="#" class="mabellas-location-btn">VISIT US</a>
+					</div>
+				</div>
+
+				<!-- MIDLAND -->
+				<div class="col-lg-6">
+					<div class="mabellas-location-card text-center h-100 p-3 text-white">
+
+						<div class="mabellas-location-image"
+							 style="background-image:url('https://visionquestdev.com/mabellas/wp-content/uploads/2025/12/EkmDKBDTRaC41Pg7v0uw_Mabellas-17.png');">
+						</div>
+
+						<h2>MIDLAND</h2>
+
+						<a href="#" class="mabellas-location-btn">VISIT US</a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+		<hr>
+		
+		<main id="primary" class="site-main mabellas-home">
+			<h2 class="text-white text-center pb-2 px-3">The "Talk" of the Town</h2>
+			<?php if ( have_posts() ) : ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php
+					// Get all homepage sections from CMB2 group.
+					$sections = get_post_meta( get_the_ID(), 'homepage_sections_repeatable_group', true );
+					?>
+
+					<?php if ( ! empty( $sections ) && is_array( $sections ) ) : ?>
+
+						<?php
+						$index = 0;
+
+						foreach ( $sections as $section ) :
+
+							// Safely pull values out of the section array.
+							$bg_value   = isset( $section['background_image'] ) ? $section['background_image'] : '';
+							$image_url  = function_exists( 'mabellas_get_cmb2_image_url' ) ? mabellas_get_cmb2_image_url( $bg_value ) : '';
+
+							$title      = isset( $section['title'] ) ? $section['title'] : '';
+							$subtitle   = isset( $section['subtitle'] ) ? $section['subtitle'] : '';
+							$paragraph  = isset( $section['paragraph'] ) ? $section['paragraph'] : '';
+							$btn_text   = isset( $section['button1_text'] ) ? $section['button1_text'] : '';
+							$btn_link   = isset( $section['button1_link'] ) ? $section['button1_link'] : '';
+
+							$is_even    = ( $index % 2 === 0 ); // even index = text left, image right
+							?>
+
+							<section class="mabellas-home-section mt-3 pb-3">
+								<div class="container-fluid">
+									<div class="row g-4 align-items-stretch">
+
+										<?php if ( $is_even ) : ?>
+
+											<!-- Text left / Image right -->
+											<div class="col-lg-4 d-flex">
+												<div class="mabellas-home-section-text pegasus-bg-dark w-100 d-flex flex-column justify-content-center text-center text-white p-5 ">
+													<?php if ( $title ) : ?>
+														<h2 class="mabellas-section-title mb-2">
+															<?php echo esc_html( $title ); ?>
+														</h2>
+													<?php endif; ?>
+
+													<?php if ( $subtitle ) : ?>
+														<p class="mabellas-section-subtitle mb-3 text-uppercase small">
+															<?php echo esc_html( $subtitle ); ?>
+														</p>
+													<?php endif; ?>
+
+													<?php if ( $paragraph ) : ?>
+														<div class="mabellas-section-paragraph mb-4">
+															<?php
+															// wysiwyg content – allow formatting.
+															echo wp_kses_post( wpautop( $paragraph ) );
+															?>
+														</div>
+													<?php endif; ?>
+
+													<?php if ( $btn_text && $btn_link ) : ?>
+														<a class="btn mabellas-btn mabellas-section-btn" href="<?php echo esc_url( $btn_link ); ?>">
+															<?php echo esc_html( $btn_text ); ?>
+														</a>
+													<?php endif; ?>
+												</div>
+											</div>
+
+											<div class="col-lg-8">
+												<div class="mabellas-home-section-image h-100 ">
+													<?php if ( $image_url ) : ?>
+														<img src="<?php echo esc_url( $image_url ); ?>" alt="" class="img-fluid w-100 h-100 ">
+													<?php endif; ?>
+												</div>
+											</div>
+
+										<?php else : ?>
+
+											<!-- Image left / Text right -->
+											<div class="col-lg-8 order-lg-1 order-2">
+												<div class="mabellas-home-section-image h-100 ">
+													<?php if ( $image_url ) : ?>
+														<img src="<?php echo esc_url( $image_url ); ?>" alt="" class="img-fluid w-100 h-100 ">
+													<?php endif; ?>
+												</div>
+											</div>
+
+											<div class="col-lg-4 order-lg-2 order-1 d-flex">
+												<div class="mabellas-home-section-text pegasus-bg-dark w-100 d-flex flex-column justify-content-center text-center text-white p-5 ">
+													<?php if ( $title ) : ?>
+														<h2 class="mabellas-section-title mb-2">
+															<?php echo esc_html( $title ); ?>
+														</h2>
+													<?php endif; ?>
+
+													<?php if ( $subtitle ) : ?>
+														<p class="mabellas-section-subtitle mb-3 text-uppercase small">
+															<?php echo esc_html( $subtitle ); ?>
+														</p>
+													<?php endif; ?>
+
+													<?php if ( $paragraph ) : ?>
+														<div class="mabellas-section-paragraph mb-4">
+															<?php echo wp_kses_post( wpautop( $paragraph ) ); ?>
+														</div>
+													<?php endif; ?>
+
+													<?php if ( $btn_text && $btn_link ) : ?>
+														<a class="btn mabellas-btn mabellas-section-btn" href="<?php echo esc_url( $btn_link ); ?>">
+															<?php echo esc_html( $btn_text ); ?>
+														</a>
+													<?php endif; ?>
+												</div>
+											</div>
+
+										<?php endif; ?>
+
+									</div><!-- .row -->
+								</div><!-- .container-fluid -->
+							</section>
+
+							<?php
+							$index++;
+						endforeach;
+						?>
+
+					<?php else : ?>
+
+						<!-- Fallback: show normal page content if no sections defined -->
+						<div class="container py-5">
+							<?php the_content(); ?>
+						</div>
+
+					<?php endif; ?>
+
+				<?php endwhile; ?>
+			<?php endif; ?>
+
+		</main>
+		
+		<section>
+			<div class="container py-5">
+				<div class="mabellas-newsletter-card text-center">
+					<h2 class="mabellas-newsletter-title">
+						Stay in the Mabella’s Loop
+					</h2>
+					<p class="mabellas-newsletter-text">
+						Sign up to receive updates on chef specials, seasonal menus, and exclusive events.
+					</p>
+
+					<form class="mabellas-newsletter-form" action="#" method="post" novalidate>
+						<div class="mabellas-newsletter-fields">
+							<label for="mabellas-newsletter-email" class="visually-hidden">
+								Email address
+							</label>
+							<input
+								type="email"
+								id="mabellas-newsletter-email"
+								name="mabellas_newsletter_email"
+								class="mabellas-newsletter-input"
+								placeholder="Enter your email address"
+								required
+							>
+							<button type="submit" class="mabellas-newsletter-btn">
+								Join the List
+							</button>
+						</div>
+						<p class="mabellas-newsletter-note">
+							We respect your privacy. No spam, just great food and good news.
+						</p>
+					</form>
+				</div>
+			</div>
+		</section>
+
+
+		<div class="<?php echo $final_container_class; ?>">
+		<!-- Example row of columns -->
+			<div class="">
+
+				<div class="inner-content">
+					<div class="content-no-sidebar">
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+							<?php if( 'off' === $final_page_header_option ) { ?>
+								<div class="page-header">
+									<?php
+									if( '' === $page_title ) {
+										echo '';
+									} elseif ( $page_title ) {
+										echo '<h1>';
+										echo the_title();
+										echo '</h1>';
+									}
+									?>
+								</div>
+							<?php }else{ ?>
+								<!--<div class="page-header-spacer"></div>-->
+							<?php } ?>
+
+							<?php the_content(); ?>
+
+						<?php endwhile; else: ?>
+							<?php /* kinda a 404 of sorts when not working */ ?>
+							<div class="page-header">
+								<h1>Oh no!</h1>
+							</div>
+							<p>No content is appearing for this page!</p>
+						<?php endif; ?>
+						<?php
+							if ( function_exists( 'wp_bootstrap_edit_post_link' ) ) {
+								// Edit post link
+								wp_bootstrap_edit_post_link(
+									sprintf(
+										/* translators: %s: Name of current post */
+										__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'pegasus' ),
+										get_the_title()
+									),
+									'<span class="edit-link">',
+									'</span>'
+								);
+							}
+							if ( function_exists( 'wp_bootstrap_posts_pagination' ) ) {
+								wp_bootstrap_posts_pagination( array(
+									'prev_text'          => __( 'Previous page', 'pegasus' ),
+									'next_text'          => __( 'Next page', 'pegasus' ),
+									'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'pegasus' ) . ' </span>'
+								) );
+							}
+						?>
+					</div>
+				</div><!--end inner content-->
+
+			</div><!--end row -->
+		</div><!-- end container -->
+	</div><!-- end page wrap -->
+    <?php get_footer(); ?>
