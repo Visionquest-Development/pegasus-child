4c379010ea3e57d6c854d3b29b96f54c5a9b29dc
Jim O'Brien <jim.obrien3@gmail.com>
2026-02-01 16:02:24 -0500

    make logo_slider global on footer of each site

---
+++ templates/logo_slider.php
@@ -0,0 +1,33 @@
+
+<div class=" ulg-logo-slider slider">
+	<figure class="logo-slide">
+		<a class="slick-slider-item" href="https://mabellas.com">
+			<img class="post-img-feat img-fluid" src="https://visionquestdev.com/ulg/wp-content/uploads/2025/12/mabellas1.png" alt="Mabellas">
+		</a>
+	</figure>
+	<figure class="logo-slide">
+		<a class="slick-slider-item" href="https://mabellas.com">
+			<img class="post-img-feat img-fluid" src="https://visionquestdev.com/ulg/wp-content/uploads/2025/12/mabellas2.png" alt="Mabellas">
+		</a>
+	</figure>
+	<figure class="logo-slide">
+		<a class="slick-slider-item" href="https://theloft.com">
+			<img class="post-img-feat img-fluid" src="https://visionquestdev.com/ulg/wp-content/uploads/2025/12/theloft.png" alt="The Loft">
+		</a>
+	</figure>
+	<figure class="logo-slide">
+		<a class="slick-slider-item" href="https://saltcellar.com">
+			<img class="post-img-feat img-fluid" src="https://visionquestdev.com/ulg/wp-content/uploads/2025/12/saltcellar.png" alt="Salt Cellar">
+		</a>
+	</figure>
+	<figure class="logo-slide">
+		<a class="slick-slider-item" href="https://themixmarket.com">
+			<img class="post-img-feat img-fluid" src="https://visionquestdev.com/ulg/wp-content/uploads/2025/12/mix-market.png" alt="The Mix Market">
+		</a>
+	</figure>
+	<figure class="logo-slide">
+		<a class="slick-slider-item" href="https://tommygs.com">
+			<img class="post-img-feat img-fluid" src="https://visionquestdev.com/ulg/wp-content/uploads/2025/12/tommygs.png" alt="TommyG's">
+		</a>
+	</figure>
+</div>
