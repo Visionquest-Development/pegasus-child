4bf8066864dcd492f0c54c9042cbb6f174cbe342
Jim O'Brien <jim.obrien3@gmail.com>
2026-02-03 02:25:25 -0500

    updating columns

--- tpl_about.php
+++ tpl_about.php
@@ -119,19 +119,15 @@
 				<div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow" style="flex-basis:100%">
 
 
-					<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
 
 					<p>At 34 Oak Contracting, we believe in transforming houses into homes through exceptional craftsmanship, attention to detail, and a commitment to excellence. Founded with a passion for quality and customer satisfaction, we have built a reputation as a trusted partner for homeowners and real estate professionals across the region.</p>
 
-					<div style="height:10px" aria-hidden="true" class="wp-block-spacer"></div>
 
 					<hr class="wp-block-separator has-alpha-channel-opacity">
 
-					<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
 
 					<h2 class="wp-block-heading">Our Story</h2>
 
-					<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
 
 					<div class="wp-block-media-text has-media-on-the-right is-stacked-on-mobile" style="grid-template-columns:auto 32%">
 						<div class="wp-block-media-text__content">
@@ -142,11 +138,8 @@
 						</figure>
 					</div>
 
-					<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
 
-					<hr class="wp-block-separator has-alpha-channel-opacity">
 
-					<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
 
 					<div class="row g-4 align-items-center">
 						<div class="col-md-4 text-center">
@@ -170,15 +163,9 @@
 						</div>
 					</div>
 
-					<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
-
-					<hr class="wp-block-separator has-alpha-channel-opacity">
-
-					<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
 
 					<h2 class="wp-block-heading">Working Together</h2>
 
-					<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
 
 					<div class="wp-block-media-text is-stacked-on-mobile" style="grid-template-columns:38% auto">
 						<figure class="wp-block-media-text__media">
@@ -187,21 +174,11 @@
 						<div class="wp-block-media-text__content">
 							<p>At 34 Oak, we pride ourselves on delivering a seamless, end-to-end client experience.</p>
 
-							<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
-
 							<p>Our knowledgeable representatives offer expert guidance during the initial consultation to bring your property vision to life.</p>
 						</div>
 					</div>
 
-					<div style="height:25px" aria-hidden="true" class="wp-block-spacer"></div>
-
-					<hr class="wp-block-separator has-alpha-channel-opacity">
-
-					<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
-
 					<h2 class="wp-block-heading has-text-align-center">Our Services</h2>
-
-					<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
 
 					<div class="row g-4">
 						<div class="col-lg-4 col-md-6">
@@ -253,15 +230,8 @@
 						</div>
 					</div>
 
-					<div style="height:30px" aria-hidden="true" class="wp-block-spacer"></div>
-
-					<hr class="wp-block-separator has-alpha-channel-opacity">
-
-					<div style="height:15px" aria-hidden="true" class="wp-block-spacer"></div>
-
 					<h2 class="wp-block-heading has-text-align-center">Why Choose Us</h2>
 
-					<div style="height:35px" aria-hidden="true" class="wp-block-spacer"></div>
 					<div class="row">
 						<div class="wp-block-media-text is-stacked-on-mobile col-md-6">
 							<figure class="wp-block-media-text__media">
@@ -291,6 +261,251 @@
 			</div>
 		</div>
 
+		<div class="container">
+			<div class="wp-block-coblocks-accordion">
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: What types of projects do you specialize in?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: We specialize in a wide range of projects, including interior repairs, exterior repairs, roofing, fencing, driveways, and pre-sale make ready repairs.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong><strong>Q: Are you licensed, bonded, and insured?</strong></strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: Absolutely, we are fully licensed, bonded, and insured to ensure your peace of mind and protect your property.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong><strong>Q: Are you licensed, bonded, and insured?</strong></strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: Absolutely, we are fully licensed, bonded, and insured to ensure your peace of mind and protect your property.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: How do I schedule a consultation or inspection?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: You can schedule a consultation or inspection by contacting us through our website, calling our office, or sending us an email. We aim to respond promptly to all inquiries.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: Do you offer free consultations and estimates?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: Yes, we offer free consultations and estimates to help you plan your project without any upfront costs.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: Are you accredited by the Better Business Bureau?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: Yes, we are accredited by the Better Business Bureau, reflecting our commitment to quality and customer satisfaction.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: What kind of warranty do you offer on roofing and siding?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: We offer a 15-year workmanship warranty on roofing and siding, ensuring long-term quality and durability.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: Are your workmanship warranties transferrable?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: Yes, all our workmanship warranties are fully transferrable, adding value to your property.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: How experienced is your leadership team?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: Our leadership team has an average of 12+ years of industry experience, bringing extensive knowledge and expertise to every project.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: How was your leadership team trained?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: 34 Oak leadership was trained by industry titans, ensuring we adhere to the highest standards of excellence.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: What areas do you serve?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: We serve a wide range of areas within our region. Please contact us to confirm if we serve your specific location.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: What sets you apart from other contractors?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: Our commitment to quality, customer satisfaction, comprehensive services, and our experienced team sets us apart. We strive to exceed expectations on every project.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: Do you handle emergency repairs?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: Yes, we handle emergency repairs to address urgent issues promptly. Please contact us immediately if you require emergency assistance.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: How long does a typical project take to complete?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: The duration of a project varies based on its size and complexity. We provide detailed timelines during the consultation phase and keep you updated throughout the project.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: How do you ensure the quality of your work?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: We ensure quality through our skilled team, use of premium materials, adherence to industry standards, and rigorous quality control processes throughout each project.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: Do you offer financing options for large projects?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: Yes, we offer flexible financing options to help you manage the costs of larger projects. Contact us for more details on our financing plans.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: Can you provide references from past clients?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: Yes, we can provide references from past clients upon request. We also have testimonials and reviews available on our website for you to review.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong>Q: How do you handle project clean-up?</strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p>A: We prioritize thorough clean-up after each project, ensuring your property is left clean and tidy. We remove all debris and materials related to the project.</p>
+						</div>
+					</details>
+				</div>
+
+				<div class="wp-block-coblocks-accordion-item">
+					<details>
+						<summary class="wp-block-coblocks-accordion-item__title"><strong><em>Have other questions? Click to see if we answer it in our extended FAQ!</em></strong></summary>
+						<div class="wp-block-coblocks-accordion-item__content">
+							<p><strong>Q: How do you communicate with clients during a project?</strong></p>
+							<p>A: We maintain clear and consistent communication throughout the project, providing regular updates and being available for any questions or concerns you may have.</p>
+
+							<p><strong>Q: Do you offer any maintenance services after project completion?</strong></p>
+							<p>A: Yes, we offer maintenance services to ensure your repairs and renovations remain in excellent condition. Contact us to learn more about our maintenance plans.</p>
+
+							<p><strong>Q: Can you help with design and planning for my project?</strong></p>
+							<p>A: Yes, we offer design and planning services to help bring your vision to life. Our team works closely with you to create a detailed plan that meets your needs and preferences.</p>
+
+							<p><strong>Q: Do you offer free storm damage inspections?</strong></p>
+							<p>A: Yes, we provide free storm damage inspections to assess and address any issues promptly.</p>
+
+							<p><strong>Q: Do you have specialists for storm damage insurance claims?</strong></p>
+							<p>A: Yes, we have trained storm damage insurance claim specialists to assist you with your claims process.</p>
+
+							<p><strong>Q: Can you cover all my construction needs with a single contact?</strong></p>
+							<p>A: Yes, we provide a single contact to cover all your construction needs, simplifying the process for you.</p>
+
+							<p><strong>Q: Do you work with skilled trades for plumbing, electrical, and carpentry?</strong></p>
+							<p>A: Yes, we partner with skilled trades offering licensed plumbing, electrical, and carpentry services.</p>
+
+							<p><strong>Q: Do you have relationships with real estate agents?</strong></p>
+							<p>A: Yes, we have working relations with top real estate agents, ensuring you have the best team to support your property investment goals.</p>
+
+							<p><strong>Q: Do you require money down before starting a project?</strong></p>
+							<p>A: No, we require zero money down prior to project commencement, allowing you to start your project with confidence.</p>
+
+							<p><strong>Q: Are you a local company?</strong></p>
+							<p>A: Yes, we are local and committed to serving our community with exceptional contracting services.</p>
+
+							<p><strong>Q: Can I count on your quality of work?</strong></p>
+							<p>A: Absolutely, we are quality contractors you can count on for all your construction needs.</p>
+						</div>
+					</details>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row g-4">
+				<div class="col-lg-4 col-md-6">
+					<figure class="wp-block-image size-large">
+						<img loading="lazy" decoding="async" width="1152" height="2048" src="https://34oakcontracting.com/wp-content/uploads/2024/05/5-1-1152x2048.png" alt="" class="wp-image-361" srcset="https://34oakcontracting.com/wp-content/uploads/2024/05/5-1-1152x2048.png 1152w, https://34oakcontracting.com/wp-content/uploads/2024/05/5-1-281x500.png 281w, https://34oakcontracting.com/wp-content/uploads/2024/05/5-1-768x1365.png 768w, https://34oakcontracting.com/wp-content/uploads/2024/05/5-1-864x1536.png 864w, https://34oakcontracting.com/wp-content/uploads/2024/05/5-1.png 1215w" sizes="auto, (max-width: 1152px) 100vw, 1152px">
+					</figure>
+				</div>
+
+				<div class="col-lg-4 col-md-6">
+					<figure class="wp-block-image size-large">
+						<img loading="lazy" decoding="async" width="1152" height="2048" src="https://34oakcontracting.com/wp-content/uploads/2024/05/4-1-1152x2048.png" alt="" class="wp-image-362" srcset="https://34oakcontracting.com/wp-content/uploads/2024/05/4-1-1152x2048.png 1152w, https://34oakcontracting.com/wp-content/uploads/2024/05/4-1-281x500.png 281w, https://34oakcontracting.com/wp-content/uploads/2024/05/4-1-768x1365.png 768w, https://34oakcontracting.com/wp-content/uploads/2024/05/4-1-864x1536.png 864w, https://34oakcontracting.com/wp-content/uploads/2024/05/4-1.png 1215w" sizes="auto, (max-width: 1152px) 100vw, 1152px">
+					</figure>
+				</div>
+
+				<div class="col-lg-4 col-md-6">
+					<figure class="wp-block-image size-large">
+						<img loading="lazy" decoding="async" width="1152" height="2048" src="https://34oakcontracting.com/wp-content/uploads/2024/05/6-1-1152x2048.png" alt="" class="wp-image-363" srcset="https://34oakcontracting.com/wp-content/uploads/2024/05/6-1-1152x2048.png 1152w, https://34oakcontracting.com/wp-content/uploads/2024/05/6-1-281x500.png 281w, https://34oakcontracting.com/wp-content/uploads/2024/05/6-1-768x1365.png 768w, https://34oakcontracting.com/wp-content/uploads/2024/05/6-1-864x1536.png 864w, https://34oakcontracting.com/wp-content/uploads/2024/05/6-1.png 1215w" sizes="auto, (max-width: 1152px) 100vw, 1152px">
+					</figure>
+				</div>
+
+				<div class="col-lg-4 col-md-6">
+					<figure class="wp-block-image size-large">
+						<img loading="lazy" decoding="async" width="1152" height="2048" src="https://34oakcontracting.com/wp-content/uploads/2024/05/7-1-1152x2048.png" alt="" class="wp-image-364" srcset="https://34oakcontracting.com/wp-content/uploads/2024/05/7-1-1152x2048.png 1152w, https://34oakcontracting.com/wp-content/uploads/2024/05/7-1-281x500.png 281w, https://34oakcontracting.com/wp-content/uploads/2024/05/7-1-768x1365.png 768w, https://34oakcontracting.com/wp-content/uploads/2024/05/7-1-864x1536.png 864w, https://34oakcontracting.com/wp-content/uploads/2024/05/7-1.png 1215w" sizes="auto, (max-width: 1152px) 100vw, 1152px">
+					</figure>
+				</div>
+
+				<div class="col-lg-4 col-md-6">
+					<figure class="wp-block-image size-large">
+						<img loading="lazy" decoding="async" width="1152" height="2048" src="https://34oakcontracting.com/wp-content/uploads/2024/05/8-1-1152x2048.png" alt="" class="wp-image-365" srcset="https://34oakcontracting.com/wp-content/uploads/2024/05/8-1-1152x2048.png 1152w, https://34oakcontracting.com/wp-content/uploads/2024/05/8-1-281x500.png 281w, https://34oakcontracting.com/wp-content/uploads/2024/05/8-1-768x1365.png 768w, https://34oakcontracting.com/wp-content/uploads/2024/05/8-1-864x1536.png 864w, https://34oakcontracting.com/wp-content/uploads/2024/05/8-1.png 1215w" sizes="auto, (max-width: 1152px) 100vw, 1152px">
+					</figure>
+				</div>
+
+				<div class="col-lg-4 col-md-6">
+					<figure class="wp-block-image size-large">
+						<img loading="lazy" decoding="async" width="1152" height="2048" src="https://34oakcontracting.com/wp-content/uploads/2024/05/9-1-1152x2048.png" alt="" class="wp-image-366" srcset="https://34oakcontracting.com/wp-content/uploads/2024/05/9-1-1152x2048.png 1152w, https://34oakcontracting.com/wp-content/uploads/2024/05/9-1-281x500.png 281w, https://34oakcontracting.com/wp-content/uploads/2024/05/9-1-768x1365.png 768w, https://34oakcontracting.com/wp-content/uploads/2024/05/9-1-864x1536.png 864w, https://34oakcontracting.com/wp-content/uploads/2024/05/9-1.png 1215w" sizes="auto, (max-width: 1152px) 100vw, 1152px">
+					</figure>
+				</div>
+			</div>
+		</div>
 
 
 
