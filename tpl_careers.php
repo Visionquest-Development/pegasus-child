5ac380b38758dd6771eba314a174c7c3733edabe
Jim O'Brien <jim.obrien3@gmail.com>
2026-02-08 21:17:57 -0500

    add careers

---
+++ tpl_careers.php
@@ -0,0 +1,153 @@
+<?php
+/*
+	Template Name: Home Template
+*/
+?>
+	<?php get_header(); ?>
+
+	<?php
+		$header_choice = pegasus_get_option( 'header_select' );
+		//var_dump($header_choice);
+		if ( 'header-three' === $header_choice ) {
+			get_template_part( 'templates/additional_header' );
+		}
+	?>
+
+	<div id="page-wrap">
+
+		<?php
+			//full container page options
+			$post_full_container_choice = get_post_meta( get_the_ID(), 'pegasus-page-container-checkbox', true );
+			//full container theme option
+			$global_full_container_option = pegasus_get_option('full_container_chk' );
+
+			//assign post class
+			$pegasus_post_container_choice = ( 'on' === $post_full_container_choice ) ? 'container-fluid' : 'container';
+			//assign global class
+			$pegasus_global_container_choice = ( 'on' === $global_full_container_option ) ? 'container-fluid' : 'container' ;
+			//check global first then post
+			$final_container_class = ( 'container-fluid' === $pegasus_global_container_choice ) ? $pegasus_global_container_choice : $pegasus_post_container_choice;
+
+			//left align right sidebar?
+			$left_align_sidebar_chk =  pegasus_get_option( 'sidebar_left_chk' ) ? pegasus_get_option( 'sidebar_left_chk' ) : 'off';
+			//enable both sidebars?
+			$pegasus_left_sidebar_option = ( 'on' === pegasus_get_option( 'both_sidebar_chk' ) ) ? pegasus_get_option( 'both_sidebar_chk' ) : 'off';
+			//change content class if both sidebars
+			$page_body_content_class = ( 'on' === $pegasus_left_sidebar_option  ) ? 'col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xg-6' : 'col-xs-12 col-sm-12 col-md-12 col-lg-9 col-xg-9';
+
+			//page header page options
+			$post_disable_page_header_choice = get_post_meta( get_the_ID(), 'pegasus-page-header-checkbox', true ) ? get_post_meta( get_the_ID(), 'pegasus-page-header-checkbox', true ) : 'off';
+			//page header theme option
+			$global_disable_page_header_option =  pegasus_get_option('page_header_chk' ) ? pegasus_get_option('page_header_chk' ) : 'off';
+			//check theme option for page header before page option
+			$page_title = $post->post_title;
+			$is_this_home = is_home();
+			if ( 'on' === $global_disable_page_header_option ) {
+				$final_page_header_option = 'on';
+			} elseif ( 'on' === $post_disable_page_header_choice ) {
+				$final_page_header_option = 'on';
+			} else {
+				$final_page_header_option = 'off';
+			}
+
+			if ( true === $is_this_home ) {
+				$final_page_header_option = 'off';
+			}
+		?>
+
+		<div class="<?php echo $final_container_class; ?>">
+		<!-- Example row of columns -->
+			<div class="">
+
+				<div class="inner-content">
+					<div class="content-no-sidebar">
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+							<?php if( 'off' === $final_page_header_option ) { ?>
+								<div class="page-header">
+									<?php
+									if( '' === $page_title ) {
+										echo '';
+									} elseif ( $page_title ) {
+										echo '<h1>';
+										echo the_title();
+										echo '</h1>';
+									}
+									?>
+								</div>
+							<?php }else{ ?>
+								<!--<div class="page-header-spacer"></div>-->
+							<?php } ?>
+
+							<?php the_content(); ?>
+
+						<?php endwhile; else: ?>
+							<?php /* kinda a 404 of sorts when not working */ ?>
+							<div class="page-header">
+								<h1>Oh no!</h1>
+							</div>
+							<p>No content is appearing for this page!</p>
+						<?php endif; ?>
+						<?php
+							if ( function_exists( 'wp_bootstrap_edit_post_link' ) ) {
+								// Edit post link
+								wp_bootstrap_edit_post_link(
+									sprintf(
+										/* translators: %s: Name of current post */
+										__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'pegasus' ),
+										get_the_title()
+									),
+									'<span class="edit-link">',
+									'</span>'
+								);
+							}
+							if ( function_exists( 'wp_bootstrap_posts_pagination' ) ) {
+								wp_bootstrap_posts_pagination( array(
+									'prev_text'          => __( 'Previous page', 'pegasus' ),
+									'next_text'          => __( 'Next page', 'pegasus' ),
+									'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'pegasus' ) . ' </span>'
+								) );
+							}
+						?>
+					</div>
+				</div><!--end inner content-->
+
+			</div><!--end row -->
+		</div><!-- end container -->
+
+		<section class="py-5">
+			<?php $restaurant_name = 'The Loft'; ?>
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-lg-10">
+						<div class="text-center mb-4">
+							<h2 class="mb-3">Careers at <?php echo $restaurant_name; ?></h2>
+							<p class="mb-0">We are always looking for passionate, hospitality-forward people who love great food, live music, and creating unforgettable experiences.</p>
+						</div>
+
+						<div class="row g-4 align-items-center">
+							<div class="col-lg-6">
+								<h3 class="h5 mb-3">Positions We Often Hire</h3>
+								<ul class="mb-4">
+									<li>Servers, bartenders, and hosts</li>
+									<li>Line cooks and prep cooks</li>
+									<li>Event and door staff</li>
+									<li>Managers and kitchen leadership</li>
+								</ul>
+								<p class="mb-4">Openings change throughout the year. Visit the ULG careers page to view current roles, expand job descriptions, and submit an application.</p>
+								<a class="btn btn-primary" href="https://uptownlifegroup.com/careers">View Careers at ULG</a>
+							</div>
+							<div class="col-lg-6">
+								<div class="p-4 bg-light h-100 rounded">
+									<h3 class="h5 mb-3">How to Apply</h3>
+									<p class="mb-3">Click through to the main ULG careers page, select the role that fits you best, and expand the listing to see full details.</p>
+									<p class="mb-0">When you are ready, complete the application form on the ULG site to be considered for <?php echo $restaurant_name; ?> and other ULG venues.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+	</div><!-- end page wrap -->
+    <?php get_footer(); ?>
