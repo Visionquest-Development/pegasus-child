3ec0d2977337ff5598319863665e2feae12c47bb
Jim O'Brien <jim.obrien3@gmail.com>
2026-02-25 16:50:55 -0500

    fix images in top bar, and images in logo slider, and add images for homepage sliders

--- templates/logo_slider.php
+++ templates/logo_slider.php
@@ -1,28 +1,42 @@
+<?php
+$mabellas_bkg_url = '/wp-content/themes/pegasus-child/images/logo_slider/mabellas_bkg.png';
+$theloft_bkg_url = '/wp-content/themes/pegasus-child/images/logo_slider/theloft_bkg.png';
+$saltcellar_bkg_url = '/wp-content/themes/pegasus-child/images/logo_slider/saltcellar_bkg.png';
+$mixmarket_bkg_url = '/wp-content/themes/pegasus-child/images/logo_slider/mix_bkg.png';
+$tommygs_bkg_url = '/wp-content/themes/pegasus-child/images/logo_slider/tommygs_bkg.png';
+
+$mabellas_white_logo_url = '/wp-content/themes/pegasus-child/images/logo_slider/mabellas_logo.png';
+$theloft_white_logo_url = '/wp-content/themes/pegasus-child/images/logo_slider/theloft_logo.png';
+$saltcellar_white_logo_url = '/wp-content/themes/pegasus-child/images/logo_slider/saltcellar_logo.png';
+$mixmarket_white_logo_url = '/wp-content/themes/pegasus-child/images/logo_slider/mix_market_logo.png';
+$tommygs_white_logo_url = '/wp-content/themes/pegasus-child/images/logo_slider/tommygs_logo.png';
+
+?>
 
 <div class=" ulg-logo-slider slider">
 	<figure class="logo-slide mabellas-logo" >
-		<a class="slick-slider-item" href="https://mabellas.com" style="background-image: url('//visionquestdev.com/mabellas/wp-content/uploads/2025/12/1RunoTqHQBmWIskGsNoM_Mabellas-18.png');">
-			<img class="post-img-feat img-fluid" src="//visionquestdev.com/mabellas/wp-content/uploads/2025/12/92014white-logo.png" alt="Mabellas">
+		<a class="slick-slider-item" href="https://mabellas.com" style="background-image: url('<?php echo $mabellas_bkg_url; ?>');">
+			<img class="post-img-feat img-fluid" src="<?php echo $mabellas_white_logo_url; ?>" alt="Mabellas">
 		</a>
 	</figure>
 	<figure class="logo-slide theloft-logo" >
-		<a class="slick-slider-item" href="https://theloft.com" style="background-image: url('//visionquestdev.com/theloft2025/wp-content/uploads/2025/12/78579364A9748.png');">
-			<img class="post-img-feat img-fluid" src="//visionquestdev.com/ulg/wp-content/uploads/2025/12/theloft.png" alt="The Loft">
+		<a class="slick-slider-item" href="https://theloft.com" style="background-image: url('<?php echo $theloft_bkg_url; ?>');">
+			<img class="post-img-feat img-fluid" src="<?php echo $theloft_white_logo_url; ?>" alt="The Loft">
 		</a>
 	</figure>
 	<figure class="logo-slide saltcellar-logo" >
-		<a class="slick-slider-item" href="https://saltcellar.com" style="background-image: url('//saltcellar.com/wp-content/uploads/2026/02/Saltcellar.png');">
-			<img class="post-img-feat img-fluid" src="//visionquestdev.com/ulg/wp-content/uploads/2025/12/saltcellar.png" alt="Salt Cellar">
+		<a class="slick-slider-item" href="https://saltcellar.com" style="background-image: url('<?php echo $saltcellar_bkg_url; ?>');">
+			<img class="post-img-feat img-fluid" src="<?php echo $saltcellar_white_logo_url; ?>" alt="Salt Cellar">
 		</a>
 	</figure>
 	<figure class="logo-slide mixmarket-logo" >
-		<a class="slick-slider-item" href="https://themixmarket.com" style="background-image: url('//visionquestdev.com/mixmarket/wp-content/uploads/2026/01/211219429f35d-02e8-4ec7-8cc8-f67.png');">
-			<img class="post-img-feat img-fluid" src="//visionquestdev.com/ulg/wp-content/uploads/2025/12/mix-market.png" alt="The Mix Market">
+		<a class="slick-slider-item" href="https://themixmarket.com" style="background-image: url('<?php echo $mixmarket_bkg_url; ?>');">
+			<img class="post-img-feat img-fluid" src="<?php echo $mixmarket_white_logo_url; ?>" alt="The Mix Market">
 		</a>
 	</figure>
 	<figure class="logo-slide tommygs-logo" >
-		<a class="slick-slider-item" href="https://tommygs.com" style="background-image: url('//tommygs.com/wp-content/uploads/2026/01/98159364A1225.png');">
-			<img class="post-img-feat img-fluid" src="//visionquestdev.com/ulg/wp-content/uploads/2025/12/tommygs.png" alt="TommyG's">
+		<a class="slick-slider-item" href="https://tommygs.com" style="background-image: url('<?php echo $tommygs_bkg_url; ?>');">
+			<img class="post-img-feat img-fluid" src="<?php echo $tommygs_white_logo_url; ?>" alt="TommyG's">
 		</a>
 	</figure>
 </div>
